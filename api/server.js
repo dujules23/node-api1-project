a96a7c46f09c9f4b3077d01d68700daf8062c59b
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-08-04 22:20:03 -0400

    completed get and post requests

--- api/server.js
+++ api/server.js
@@ -1,5 +1,6 @@
 // BUILD YOUR SERVER HERE
 const express = require("express")
+const User = require("../api/users/model")
 
 const server = express()
 
@@ -7,7 +8,39 @@ server.use(express.json())
 // set module.exports to export server
 module.exports = server; // EXPORT YOUR SERVER instead of {}
 
+//get - retrieves users
+server.get("/api/users", (req,res) => {
+  User.find()
+    .then(users => {
+      console.log(users)
+      res.status(200).json(users)
+    })
+    .catch(err => {
+      res.status(500).json({message: "The users information could not be retrieved", err})
+    })
+
+})
+
+
+//post - Update user
+server.post("/api/users", (req,res) => {
+  const newUser = req.body
+  if(!newUser.name || !newUser.bio){
+    res.status(400).json({message: "Please provide name and bio for the user"})
+  }
+  User.insert(newUser)
+    .then(user => {
+      res.json(user)
+      res.status(201).json(user)
+    })
+    .catch(err => {
+      res.status(500).json({message: "There was an error while saving the user to the database", err})
+    })
+})
+
+
+
 //Hello world
 server.use("*", (req,res)=> {
-  res.status(200).json({message: "hello there"})
+  res.status(404).json({message: "hello there"})
 })
--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
-server.listen(5000, ()=> {
+server.listen(port, ()=> {
   console.log("Sever up and running on port 5000")
 })
   
