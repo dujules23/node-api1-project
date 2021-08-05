cb62612ae5c64c9eea8863d6ff1c0cfa2a50b87c
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-08-05 00:17:33 -0400

    completed delete request, works as intended

--- api/server.js
+++ api/server.js
@@ -40,7 +40,7 @@ server.get("/api/users/:id", (req, res) => {
 })
 
 
-//post - Update user
+//post - Adding a new user
 server.post("/api/users", (req,res) => {
   const newUser = req.body
   if(!newUser.name || !newUser.bio){
@@ -54,6 +54,49 @@ server.post("/api/users", (req,res) => {
     .catch(err => {
       res.status(500).json({message: "There was an error while saving the user to the database", err})
     })
+})
+
+//Put request - Edits/updates an existing user
+server.put("/api/users/:id", async (req,res) => {
+  const { id } = req.params
+  const changes = req.body
+
+  try {
+    if(!changes.name || !changes.bio){
+      res.status(400).json({ message: "Please provide name and bio for the user" })
+    }
+    else{
+      const updatedUser = await User.update(id, changes)
+      if(!updatedUser){
+        res.status(404).json({message: "The user with the specified ID does not exist"})
+      }
+    }
+    const updatedUser = await User.update(id, changes)
+    res.status(200).json(updatedUser)
+  }
+  catch(err){
+    res.status(500).json({message: "The user information could not be modified"})
+  }
+})
+
+// Delete request - removes a user
+server.delete("/api/users/:id", async (req, res) => {
+  try{
+    const {id} = req.params
+    const deletedUser = await User.remove(id)
+    if(!deletedUser){
+      res.status(404).json({message: "The user with the specified ID does not exist"})
+    }
+    else{
+      res.status(201).json(deletedUser)
+    }
+    
+  }
+  catch(err){
+    res.status(500).json({ message: "The user could not be removed" })
+  }
+  
+
 })
 
 
