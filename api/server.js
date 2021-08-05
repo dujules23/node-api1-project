b8cbc8d66a93fc16323fa168c3214f58d908c4f9
Durrell Jules <durrelljules@Durrells-Mini.attlocal.net>
2021-08-04 21:38:15 -0400

    Installed express, nodemon, set up server, hello world is working

--- api/server.js
+++ api/server.js
@@ -1,3 +1,13 @@
 // BUILD YOUR SERVER HERE
+const express = require("express")
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const server = express()
+
+server.use(express.json())
+// set module.exports to export server
+module.exports = server; // EXPORT YOUR SERVER instead of {}
+
+//Hello world
+server.use("*", (req,res)=> {
+  res.status(200).json({message: "hello there"})
+})
--- index.js
+++ index.js
@@ -3,3 +3,7 @@ const server = require('./api/server');
 const port = 5000;
 
 // START YOUR SERVER HERE
+server.listen(5000, ()=> {
+  console.log("Sever up and running on port 5000")
+})
+  
